87380282829d948bc8fbd1e989dcb8244f17a3a2
JinWoo Choi <snowyowl981@gmail.com>
2021-04-16 11:35:26 +0900

    프로젝트 업로드

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Test");
     }
 
     // Update is called once per frame
